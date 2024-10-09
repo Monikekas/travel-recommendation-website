30a8363841ba3c4f48bdee81bba851d253b8079b
monikekas <monikekas@hotmail.com>
2024-10-09 17:00:12 -0400

    cambios

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,15 +1,25 @@
 let seccion = [];
-const seccion_name = ['Home', 'About us', 'Contact us'];
+const seccion_name = [
+    {text: 'Home', valor:null}, 
+    {text: 'About us', valor: null}, 
+    {text: 'Contact us', valor: null}
+];
 const ulNavBar = document.getElementById('navbarmenu');
-generateMenu()
+const contenido = document.getElementById('contenedor_contenido');
+generateMenu();
+ulNavBar.addEventListener('click', handleMenuClick);
+
+function handleMenuClick(event){
+    console.log(event.target)
+    const btn = event.target;
+}
+
 
 function generateMenu(){
-    
     for (const section of seccion_name){
         const li_section = document.createElement('li');
-        const goto_li = document.createElement('a');
-        goto_li.textContent = section;
-        li_section.appendChild(goto_li);
+        li_section.textContent = section.text;
+        li_section.classList.add = 'menu'
         ulNavBar.appendChild(li_section);
     }
 }
